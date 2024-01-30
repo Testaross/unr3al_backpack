0285b46c07c383560b625160b603ba9a80ebe7eb
Testarossa <austin.w.burrows@gmail.com>
2024-01-30 00:51:43 -0500

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,6 +1,7 @@
 local registeredStashes = {}
 local ox_inventory = exports.ox_inventory
 local count_bagpacks = 0
+local countbagpacks = 0
 
 local function GenerateText(num)
 	local str
@@ -36,8 +37,7 @@ lib.callback.register('unr3al_backpack:getNewIdentifier', function(source, slot,
 	return newId
 end)
 
-CreateThread(function()
-	while GetResourceState('ox_inventory') ~= 'started' do Wait(500) end
+repeat Wait(500) until GetResourceState('ox_inventory') == 'started'
 	if Config.Debug then print("Inventory Started") end
 	local swapHook = ox_inventory:registerHook('swapItems', function(payload)
 		local start, destination, move_type, count_bagpacks = payload.fromInventory, payload.toInventory, payload.tobagtype, 0
@@ -51,7 +51,7 @@ CreateThread(function()
 			return false
 		end
 		if Config.OneBagInInventory then
-			if (count_bagpacks > 0 and move_type == 'player' and destination ~= start) then
+			if (count_bagpacks > 0 and destination ~= start) then
 				TriggerClientEvent('ox_lib:notify', payload.source, {type = 'error', title = Strings.action_incomplete, description = Strings.one_backpack_only}) 
 				return false
 			end
@@ -62,105 +62,43 @@ CreateThread(function()
 		print = false,
 		Config.Filter
 	})
-
-	local createHook
-	createHook = exports.ox_inventory:registerHook('createItem', function(payload)
-		local count_bagpacks = 0
-
-		for vbag in pairs(Config.Backpacks) do
-			count_bagpacks = ox_inventory:GetItem(payload.inventoryId, vbag, nil, true)
-			local playerItems = ox_inventory:GetInventoryItems(payload.inventoryId)
 	
-	
-			if count_bagpacks > 0 then
-				local slot = nil
-	
-				for i,k in pairs(playerItems) do
-					if k.name == vbag then
-						slot = k.slot
-						break
-					end
-				end
-	
-				Citizen.CreateThread(function()
-					local inventoryId = payload.inventoryId
-					local dontRemove = slot
-					Citizen.Wait(1000)
-	
-					for i,k in pairs(ox_inventory:GetInventoryItems(inventoryId)) do
-						if k.name == vbag and dontRemove ~= nil and k.slot ~= dontRemove then
-							local success = ox_inventory:RemoveItem(inventoryId, vbag, 1, nil, k.slot)
-							if success then
-								TriggerClientEvent('ox_lib:notify', inventoryId, {type = 'error', title = Strings.action_incomplete, description = Strings.one_backpack_only}) 
-							end
-							break
-						end
-					end
-				end)
-			end
+	local buyHook = exports.ox_inventory:registerHook('buyItem', function(payload)
+		local inventoryId = payload.inventoryId
+		for vbag, _ in pairs(Config.Backpacks) do
+			countbagpacks = countbagpacks + ox_inventory:GetItem(payload.source, vbag, nil, true)
 		end
-
---[[
-		print(ox_inventory:GetItemCount(payload.inventoryId, "bag1", nil, false))
-		for vbag in pairs(Config.Backpacks) do
-			--count_bagpacks = ox_inventory:GetItem(payload.inventoryId, vbag, nil, true)
-			count_bagpacks = ox_inventory:GetItemCount(payload.inventoryId, vbag, nil, false)
-			local count_backpacks = ox_inventory:GetItemCount(payload.inventoryId, vbag, nil, false)
-			print("count: "..count_bagpacks)
-			print("vbag: "..vbag)
-			if count_backpacks >= 1 then
-				bag_pack = vbag
-			end
+		if countbagpacks > 0 then
+			TriggerClientEvent('ox_lib:notify', payload.source, {type = 'error', title = Strings.action_incomplete, description = Strings.one_backpack_only}) 
+			return false
 		end
-		print("count: "..count_bagpacks)
-		local playerItems = ox_inventory:GetInventoryItems(payload.inventoryId)
-
-		if count_bagpacks > 0 then
-			local slot = nil
-
-			for i,k in pairs(playerItems) do
-				if k.name == bag_pack then
-					slot = k.slot
-					break
-				end
-			end
-
-			Citizen.CreateThread(function()
-				local inventoryId = payload.inventoryId
-				local dontRemove = slot
-				Citizen.Wait(1000)
-
-				for i,k in pairs(ox_inventory:GetInventoryItems(inventoryId)) do
-					if k.name == bag_pack and dontRemove ~= nil and k.slot ~= dontRemove then
-						local success = ox_inventory:RemoveItem(inventoryId, bag_pack, 1, nil, k.slot)
-						if success then
-							TriggerClientEvent('ox_lib:notify', inventoryId, {type = 'error', title = Strings.action_incomplete, description = Strings.one_backpack_only}) 
-						end
-						break
-					end
-				end
-			end)
-		end
---]]
-
+		return true
 	end, {
-		print = false,
+		print = true,
 		Config.Filter
 	})
 
-	AddEventHandler('onResourceStop', function()
-		ox_inventory:removeHooks(swapHook)
-		ox_inventory:removeHooks(createHook)
+
+AddEventHandler('onResourceStop', function()
+	ox_inventory:removeHooks(swapHook)
+	ox_inventory:removeHooks(buyHook)
+end)
+
+if Config.Framework == 'ESX' then 
+	ESX = exports["es_extended"]:getSharedObject()
+	RegisterServerEvent('unr3al_backpack:save')
+	AddEventHandler('unr3al_backpack:save', function(skin)
+		local src = source
+		local xPlayer = ESX.GetPlayerFromId(src)
+
+		MySQL.update('UPDATE users SET skin = @skin WHERE identifier = @identifier', {
+			['@skin'] = json.encode(skin),
+			['@identifier'] = xPlayer.identifier
+		})
 	end)
-end)
+elseif Config.Framework == 'ND' then
 
-RegisterServerEvent('unr3al_backpack:save')
-AddEventHandler('unr3al_backpack:save', function(skin)
-    local src = source
-	local xPlayer = ESX.GetPlayerFromId(src)
 
-	MySQL.update('UPDATE users SET skin = @skin WHERE identifier = @identifier', {
-		['@skin'] = json.encode(skin),
-		['@identifier'] = xPlayer.identifier
-	})
-end)
+elseif Config.Framework == 'OX' then
+elseif Config.Framework == 'QB' then
+end
